8389c77c26359beaeb102da5e7439b9aa8e7ed85
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2022-06-26 04:55:38 +0200

    Char count refcator

--- js/char-count-in-string.js
+++ js/char-count-in-string.js
@@ -1,12 +1,13 @@
 function charCount(str) {
   const result = {};
 
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i];
-    if (result[char] > 0) {
-      result[char]++;
-    } else {
-      result[char] = 1;
+  for (let char of str) {
+    char = char.toLowerCase();
+    if (/[a-z0-9]/.test(char)) {
+      result[char] = ++result[char] || 1;
     }
   }
+  return result;
 }
+
+console.log(charCount("Hello.. ! h"));
