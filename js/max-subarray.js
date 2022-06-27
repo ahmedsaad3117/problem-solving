20c4cd545455565e9a29bc3fecffb58f76b403ad
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2022-06-27 11:43:07 +0200

    max - subarray

---
+++ js/max-subarray.js
@@ -0,0 +1,24 @@
+// Write a function that is accept array of integers and a number called n,
+// the function should calculate the maximum sum of n consecutive elements in the array
+
+function maxSubArraySum(arr, n) {
+  if (arr.length < n) return;
+
+  let maxSum = 0;
+  let tempSum = 0;
+  
+  for (let i = 0; i < n; i++) {
+    maxSum += arr[i];
+  }
+  for (let i = 1; i < arr.length - n + 1; i++) {
+    tempSum = maxSum + arr[n + i - 1] - arr[i - 1];
+
+    console.log(maxSum, tempSum);
+    maxSum = Math.max(tempSum, maxSum);
+  }
+  return maxSum;
+}
+
+const res = maxSubArraySum([5, 1, 3, 6], 3);
+
+console.log(res);
