689b381c10d69f1f87c72f35801600674e1a83a4
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2022-07-13 00:01:42 +0200

    candy

--- js/candy-leetCode.js
+++ js/candy-leetCode.js
@@ -6,3 +6,37 @@
 // Children with a higher rating get more candies than their neighbors.
 // Return the minimum number of candies you need to have to distribute the candies to the children. -->
 
+function candys(arr) {
+  const candys =  new Array(arr.length);
+  let temp = Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    console.log(candys);
+
+    const minIndex = arr.indexOf(Math.min(...arr)); //  Math.min(...arr)
+    if (
+      arr[minIndex] > arr[minIndex + 1] ||
+      arr[minIndex] > arr[minIndex - 1] ||
+      arr[minIndex] > temp
+    ) {
+      candys[minIndex + 1] > candys[minIndex - 1]
+        ? (candys[minIndex] = candys[minIndex + 1] + 1 || 2)
+        : (candys[minIndex] = candys[minIndex - 1] + 1 || 2);
+
+      temp = arr[minIndex];
+      arr[minIndex] = Infinity;
+    } else {
+      candys[minIndex] = 1;
+      temp = arr[minIndex];
+      arr[minIndex] = Infinity;
+    }
+  }0
+  const sum = candys.reduce((partialSum, a) => partialSum + a, 0);
+
+  console.log(candys)
+  return sum;
+}
+
+console.log(candys([1,3,2,2,1]));
+
+console.log(5 > undefined++)
